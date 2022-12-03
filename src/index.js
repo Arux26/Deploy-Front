20493a0e48465d059c5ed2399399dbcccc81753d
Arux26 <arieltrangoni1@gmail.com>
2022-12-03 15:41:21 -0300

    Deploy-F

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 import store from "./store"
+import axios from 'axios';
+
+//axios.defaults.baseURL = 'http://localhost:3001'; // lo q hacemos con esta linea es avisarle a axios q todas las peticiones q vaya a hacer empiecen
+//                                                   o tengan una misma baseURL es decir ---> http://localhost:3001
+axios.defaults.baseURL = 'https://deploy-back-production-0fc5.up.railway.app/';
+
+// SI QUEREMOS TRABAJAR DE FORMA LOCAL DESCOMENTAMOS LA LINEA DE ARRIBA Y COMENTAMOS LA DE LA LINEA 13
 
 ReactDOM.render(
   <Provider store={store}>
